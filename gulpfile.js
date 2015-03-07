501420a3f02b5b5eb4d0dfb0555597fd36bc2bf7
Claudina Sarahe <its@misscs.me>
2015-03-07 13:42:06 -0500

    Add bower
    * add normalize and queries plugins
    * remove normalize from repo; link to output in plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 
+var bower = require('bower');
+var bowerFiles = require('main-bower-files');
+var del = require('del');
 var browserSync = require('browser-sync');
 var cache = require('gulp-cached');
 var harp = require('harp');
@@ -12,7 +15,7 @@ var paths = {
   sass: ['assets/stylesheets/**/*.scss', 'assets/stylesheets/*.scss'],
   images: 'assets/img/**/*',
   js: 'assets/js/**/*.js',
-  fonts: 'assets/fonts/**/*',
+  fonts: 'assets/fonts/**/*'
 };
 
 
@@ -24,14 +27,9 @@ gulp.task('serve', function () {
     port: 9000
   }, function () {
     browserSync({
-      proxy: "localhost:9000",
+      proxy: 'localhost:9000',
       open: false,
-      ghostMode: {
-        clicks: true,
-        forms: false,
-        scroll: true
-      },
-      logLevel: "debug",
+      logLevel: 'debug',
       logPrefix: 'GothamSass',
       notify: false
     });
@@ -54,10 +52,37 @@ gulp.task('serve', function () {
 gulp.task('lint', function() {
   gulp.src(paths.sass)
     .pipe(cache('scsslint'))
-    .pipe(scsslint({'config': 'lint.yml',}))
-    .pipe(reload({stream: true}));
+    .pipe(scsslint({ 'config': 'lint.yml' }))
+    .pipe(reload({ stream: true }));
 });
 
+/**
+  * Bower tasks for Sass files and utility helpers
+  * move files from  `bower_components` to`sass/plugins/blower`
+**/
+
+gulp.task('bower-clean-sass', function (cb) {
+  del(['assets/stylesheets/plugins/bower'], cb);
+});
+
+gulp.task('bower-clean', ['bower-clean-sass']);
+
+gulp.task('bower-install', function (cb) {
+  bower.commands.install([])
+    .on('end', function () {
+      cb();
+    });
+});
+
+gulp.task('bower-sass', ['bower-clean-sass', 'bower-install'], function () {
+  return gulp.src(bowerFiles({
+      includeDev: true,
+      filter: ['**/*.scss', '**/*.css'],
+    }), {base: 'bower_components'})
+    .pipe(gulp.dest('assets/stylesheets/plugins/bower'));
+});
+
+gulp.task('bower', ['bower-sass']);
 
 /**
  * Default task, running just `gulp` will compile the sass,
